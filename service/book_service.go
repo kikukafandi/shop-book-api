2b63b5f79c3b5a979cbae386dfaa414887ae137b
kikukafandi <jtierta@gmail.com>
2025-08-24 00:16:07 +0700

    fead: add BookService Interface

--- service/book_service.go
+++ service/book_service.go
@@ -1,1 +1,11 @@
 package service
+
+import "context"
+
+type BookService interface {
+	Create(ctx context.Context)
+	Update(ctx context.Context)
+	Delete(ctx context.Context)
+	FindById(ctx context.Context)
+	FindAll(ctx context.Context)
+}
