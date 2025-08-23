3453078894a60aabfd9adabcebe59cd1002aa159
kikukafandi <jtierta@gmail.com>
2025-08-24 00:13:48 +0700

    feat: using helper for error condition

--- repository/book_repository_impl.go
+++ repository/book_repository_impl.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"context"
+	"kikukafandi/book-shop-api/helper"
 	"kikukafandi/book-shop-api/model/domain"
 
 	"gorm.io/gorm"
@@ -12,9 +13,7 @@ type BookRepositoryImpl struct {
 
 func (repository *BookRepositoryImpl) Save(ctx context.Context, db *gorm.DB, book domain.Book) (domain.Book, error) {
 	err := db.WithContext(ctx).Save(&book).Error
-	if err != nil {
-		return book, err
-	}
+	helper.PanicIfError(err)
 
 	return book, nil
 }
@@ -22,17 +21,22 @@ func (repository *BookRepositoryImpl) Save(ctx context.Context, db *gorm.DB, boo
 func (repository *BookRepositoryImpl) FindAll(ctx context.Context, db *gorm.DB) ([]domain.Book, error) {
 	var books []domain.Book
 	err := db.WithContext(ctx).Find(&books).Error
-	if err != nil {
-		return nil, err
-	}
+	helper.PanicIfError(err)
 	return books, nil
 }
 
 func (repository *BookRepositoryImpl) FindById(ctx context.Context, db *gorm.DB, bookId uint) (domain.Book, error) {
 	var book domain.Book
 	err := db.WithContext(ctx).First(&book, bookId).Error
-	if err != nil {
-		return book, err
-	}
+	helper.PanicIfError(err)
 	return book, nil
+}
+func (repository *BookRepositoryImpl) Update(ctx context.Context, db *gorm.DB, book domain.Book) (domain.Book, error) {
+	err := db.WithContext(ctx).Save(&book).Error
+	helper.PanicIfError(err)
+	return book, nil
+}
+func (repository *BookRepositoryImpl) Delete(ctx context.Context, db *gorm.DB, book domain.Book) {
+	err := db.WithContext(ctx).Delete(&book).Error
+	helper.PanicIfError(err)
 }
